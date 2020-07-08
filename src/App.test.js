d5f077f6fd341118556917a11188cd7beed5e198
Tony Mullen <tony.mullen@gmail.com>
2020-07-08 10:51:53 -0700

    Switch out require.context to use plain requires for card images

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders top level app', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/DEAL/i);
   expect(linkElement).toBeInTheDocument();
 });
