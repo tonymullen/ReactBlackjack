e55b9e4b6dea058e1a58a913a386c808b3cd3146
Tony Mullen <tony.mullen@gmail.com>
2020-07-08 10:54:32 -0700

    Switch out require.context for plain requires for card images

--- src/components/CardView.jsx
+++ src/components/CardView.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import Flippy, { FrontSide, BackSide } from 'react-flippy';
 
-const images = require.context('../assets/png/1x', true);
+const suits = ['heart', 'diamond', 'spade', 'club'];
+const faces =  [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
+const images = {}
+suits.forEach(suit => {
+  faces.forEach(face => {
+    images[`./${face}_${suit}.png`] = require(`../assets/png/1x/${face}_${suit}.png`);
+  })
+})
+const cardBack = require('../assets/png/1x/back-blue.png');
 
 const CardView = ({ card:{face, suit, flip} }) => (
   <Flippy isFlipped={flip} >
     <FrontSide>
-      <img src={images(`./${face}_${suit}.png`)} alt={`${face} of ${suit}`} />
+      <img src={images[`./${face}_${suit}.png`]} alt={`${face} of ${suit}`} />
     </FrontSide>
     <BackSide>
-      <img src={images(`./back-blue.png`)} alt="" />
+      <img src={cardBack} alt="" />
     </BackSide>
   </Flippy>
 );
